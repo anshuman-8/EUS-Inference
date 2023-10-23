8204004b951d3e9532964fe8c142b6acf8317477
Anshuman Swain <anshuman8swain@gmail.com>
2023-10-23 19:52:40 +0530

    feat:skip dopppler frames

--- testing/app.py
+++ testing/app.py
@@ -2,11 +2,14 @@ import cv2
 import torch
 import time
 import logging as log
+import numpy as np
 from PIL import Image
 from model import TestLightningModule
 from torchvision import transforms
 import torch.nn.functional as F
-from datetime import datetime, timedelta
+from datetime import timedelta
+from sklearn.metrics import balanced_accuracy_score
+
 
 
 log.basicConfig(
@@ -16,11 +19,6 @@ log.basicConfig(
     handlers=[log.FileHandler("testing.log", mode="w"), log.StreamHandler()],
 )
 
-LIVE_INFERENCE = False
-INFERENCE = True
-
-FPS = 1 if LIVE_INFERENCE else 300
-
 EUS_model_1 = "./testing/checkpoint/a7a72f80-fd9d-4f60-9cc9-1c2227375e39.ckpt"  # densenet161
 EUS_model_2 = "./testing/checkpoint/35f62346-a69b-4f7d-9e5f-b384bd2f7e16.ckpt"  # resnet50
 EUS_model_3 = "./testing/checkpoint/96c22c44-cb77-4347-9da3-052ef63437a0.ckpt"  # densenet161 denoising
@@ -28,59 +26,78 @@ EUS_model_4 = "./testing/checkpoint/d72fb346-f652-4605-9039-856ca4315bc2.ckpt"  
 
 Vid_source = "./testing/data/achamma_paulose_1967060.mkv"
 
-# Crop details
-# left, right, top, bottom
-crop_params = [185, 105, 93, 90]
-
-ground_truth = [
-    {'class':'Station 1', 'start': '00:01:09', 'end': '00:08:21'},
-    {'class':'Station 2', 'start': '00:08:44', 'end': '00:13:24'},
-    {'class':'Station 3', 'start': '00:00:00', 'end': '00:00:00'}
-]
-
 '''
 Video details:
-.mkv - New Fujifilm
-.asf - Old Fujifilm
-.avi - Olympus
 
 OBS Link:
 Vid Src 4 - New Fujifilm
 
-Vid Source:
-video_mammen.mkv [140, 420, 105, 103] 
-achamma_paulose_1967060 [0 0 0 0]
-
 Video Inference Ground Truth:
 
-achamma_paulose_1967060.asf
+achamma_paulose_1967060.asf (Old Olympus)
+src - ./testing/data/achamma_paulose_1967060.mkv
+crop_params = [185, 105, 93, 90] 
+trained - 
+test - 
 Station 1 - 00:01:09 - 00:08:21
 Station 2 - 00:08:44 - 00:13:24
 Station 3 - 00:00:00 - 00:00:00
+acc - 64.67%
+balanced acc - 77.43%
+-------------------------------------
+
+Devamma 1931981 EUS.avi (Old Olympus)
+src - ./testing/data/Devamma 1931981 EUS.avi
+crop_params = [185, 105, 93, 90] 
+trained - no
+test - yes
+Station 1 - 00:15:33 - 00:19:12
+Station 2 - 00:00:00 - 00:08:40
+Station 3 - 00:09:07 - 00:10:21
+Station 3 - 00:12:26 - 00:13:00
+acc - 64.05%
+balanced acc - 64.88%
+-------------------------------------
 
 video_mammen.mkv
+crop_params = [140, 420, 105, 103]
+trained -
+test -
 Station 1 - 00:00:00 - 00:00:00
 Station 2 - 00:00:00 - 00:00:00
 Station 3 - 00:00:00 - 00:00:00
+acc - 0.00%
+balanced acc - 0.00%
 '''
 
+# Config
+LIVE_INFERENCE = False
 cv_src = 2 if LIVE_INFERENCE else Vid_source  # 2 for HDMI port
+IMAGE_SIZE = 224
+crop_params = [185, 120, 100, 90] # left, right, top, bottom
+end_time = 1 + 60 * 15 # 20 minutes
+
+ground_truth = [
+    {'class':'1', 'start': '00:15:33', 'end': '00:19:12'},
+    {'class':'2', 'start': '00:00:00', 'end': '00:08:40'},
+    {'class':'3', 'start': '00:09:07', 'end': '00:10:21'},
+    {'class':'3', 'start': '00:12:26', 'end': '00:13:00'}
+]
+
 
 cap = cv2.VideoCapture(cv_src, cv2.CAP_FFMPEG)
 
-log.debug(f'Video capture fps: {cv2.CAP_PROP_FPS}')
-# cap.set(FPS, 350*1000)
-capture_fps = int(cap.get(cv2.CAP_PROP_FPS))
-print(capture_fps)
-width, height = int(cap.get(3)), int(cap.get(4))
 log.info("Video capture object initialized.")
+
+capture_fps = int(cap.get(cv2.CAP_PROP_FPS))
+log.info(f"Capture FPS: {capture_fps}")
+
+width, height = int(cap.get(3)), int(cap.get(4))
 
 if not cap.isOpened():
     log.error("Could not access the video source.")
     exit()
 
-
-IMAGE_SIZE = 224
 
 transforms = transforms.Compose(
     [
@@ -99,16 +116,77 @@ def preprocess_frame(frame):
     frame = Image.fromarray(frame)
     return transforms(frame)
 
-def balanced_accuracy(y_true, y_pred):
-    unique_classes = set(y_true)
-    sensitivity = []
+def avg_pixel(image):
+    average = image.mean()
+    return average
 
-    for c in unique_classes:
-        true_positive = sum((y_true == c) & (y_pred == c))
-        actual_class_count = sum(y_true == c)
-        sensitivity.append(true_positive / actual_class_count)
 
-    return sum(sensitivity) / len(unique_classes)
+def red_pixel_avg(image, hsv):
+    lower_red = np.array([-10, 100, 100])
+    upper_red = np.array([10, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(image, image, mask=mask)
+
+    average = np.mean(res)
+
+    return average
+
+
+def orange_pixel_avg(image, hsv):
+
+    lower_orange = np.array([5, 100, 100])
+    upper_orange = np.array([25, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_orange, upper_orange)
+    res = cv2.bitwise_and(image, image, mask=mask)
+
+    average = np.mean(res)
+
+    return average
+
+
+def blue_pixel_sum(image, hsv):
+
+    lower_blue = np.array([100, 150, 0], np.uint8)
+    upper_blue = np.array([140, 255, 255], np.uint8)
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    res = cv2.bitwise_and(image, image, mask=mask)
+
+    average = np.mean(res)
+
+    return average
+
+def avg_green(image, hsv):
+
+    lower_green = np.array([36, 25, 25], np.uint8)
+    upper_green = np.array([70, 255, 255], np.uint8)
+
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    green_pixels = cv2.bitwise_and(image, image, mask=mask)
+
+    average = np.mean(green_pixels)
+
+    return average
+
+def isDisturbed(frame):
+    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    if (red_pixel_avg(frame, hsv_image) >= 0.00068 or blue_pixel_sum(frame, hsv_image) >= 0.0094):
+        return True, "Doppler"
+    
+    # if (red_pixel_avg(frame, hsv_image) >= 20 or orange_pixel_avg(frame, hsv_image) >= 20):
+    #     return True, "Camera feed"
+    
+    if (avg_pixel(frame) < 12):
+        return True, "Black frame"
+    
+    if (avg_green(frame, hsv_image) > 0.21):
+        return True, "Green Cursor"
+        
+    return False, 'None'
+
 
 # Convert ground truth times to seconds
 for entry in ground_truth:
@@ -127,68 +205,87 @@ except Exception as e:
     log.error(f"Unable to load model.\n{e}")
     exit()
 
-station_class = {0: "Station 1", 1: "Station 2", 2: "Station 3"}
+station_class = {0: '1', 1: '2', 2: '3'}
 
 correct_predictions = 0
 total_predictions = 0
+actual = []
+predicted = []
 
-if INFERENCE:
-    with torch.no_grad():
+with torch.no_grad():
         start_time = time.time()
         init_time = 0
-        while cap.isOpened():
-            # ret = cap.grab()
-            ret, frame = cap.read()
-            left, right, top, bottom = crop_params
-            frame = frame[top:height - bottom, left:width - right]
 
-            current_time = cap.get(cv2.CAP_PROP_POS_MSEC) 
-            current_time = timedelta(milliseconds=current_time)
-            current_time_format = f"{current_time.seconds//3600:02}:{(current_time.seconds%3600)//60:02}:{current_time.seconds%60:02}"
+        while cap.isOpened():
+            ret, frame = cap.read()
 
             if not ret:
                 log.error(" Unable to capture frame.")
                 break
 
-            frame_tensor = preprocess_frame(frame)
+            left, right, top, bottom = crop_params
+            frame = frame[top:height - bottom, left:width - right]
 
-            prediction = model(frame_tensor.unsqueeze(0)).squeeze(0).softmax(0)
-            prediction_class = prediction.argmax().item()
-            log.info(station_class[prediction_class])
-            log.debug(f'{current_time.seconds=}')
-            for entry in ground_truth:
-                if entry['start'] <= current_time.total_seconds() <= entry['end']:
-                    if station_class[prediction_class] == entry['class']:
-                        log.info(f"Correct prediction: {station_class[prediction_class]}")
-                        correct_predictions += 1
-                    else:
-                        log.info(f"Incorrect prediction: {station_class[prediction_class]}")
-                    total_predictions += 1
 
-            for i in range(3):
-                text = f"{station_class[i]}: {prediction[i].item()*100:.2f}%"
-                position = (10, (i + 1) * 32)
-                color = (255, 255, 255)
-                cv2.putText(
-                    frame,
-                    text,
-                    position,
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    color,
-                    2,
-                    cv2.LINE_AA,
-                )
+            current_time = cap.get(cv2.CAP_PROP_POS_MSEC) 
+            current_time = timedelta(milliseconds=current_time)
+            current_time_format = f"{current_time.seconds//3600:02}:{(current_time.seconds%3600)//60:02}:{current_time.seconds%60:02}"
+
+            
+            if current_time.seconds > end_time:
+                log.info("End of video reached.")
+                log.info(f"Accuracy: {correct_predictions/total_predictions*100:.2f}%")
+                break
+
             cv2.putText(
                     frame,
                     current_time_format,
-                    (width - 370, height - 200),
+                    (width - 387, height - 200),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.6,
                     (255, 255, 255),
                     1,
                     cv2.LINE_AA,
                 )
+
+            disturbance, reason = isDisturbed(frame)
+
+            if not disturbance:
+                frame_tensor = preprocess_frame(frame)
+
+                prediction = model(frame_tensor.unsqueeze(0)).squeeze(0).softmax(0)
+                prediction_class = prediction.argmax().item()
+                log.info(f': Station {station_class[prediction_class]} - at {current_time_format}')
+
+                for entry in ground_truth:
+                    if entry['start'] <= current_time.total_seconds() <= entry['end']:
+                        if station_class[prediction_class] == entry['class']:
+                            log.info(f"Correct prediction: {station_class[prediction_class]}")
+                            correct_predictions += 1
+                        else:
+                            log.info(f"Incorrect prediction: {station_class[prediction_class]}, actual: {entry['class']}")
+                        
+                        actual.append(entry['class'])
+                        predicted.append(station_class[prediction_class])
+                        total_predictions += 1
+
+                for i in range(3):
+                    text = f"Station {station_class[i]}: {prediction[i].item()*100:.2f}%"
+                    position = (10, (i + 1) * 30)
+                    color = (255, 255, 255)
+                    cv2.putText(
+                        frame,
+                        text,
+                        position,
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7,
+                        color,
+                        1,
+                        cv2.LINE_AA,
+                    )
+            else:
+                log.info(f"Disturbed frame. Skipping frame. Reason: {reason}")
+        
 
             cv2.imshow("EUS Inference", frame)
 
@@ -203,36 +300,9 @@ if INFERENCE:
             if cv2.waitKey(capture_fps) & 0xFF == ord("q"):
                 log.info("Exiting.")
                 break
-else:
-    while cap.isOpened():
-        ret = cap.grab()
-        ret, frame = cap.retrieve()
-        left, right, top, bottom = crop_params
-        frame = frame[top:height - bottom, left:width - right]
-
-        current_time = cap.get(cv2.CAP_PROP_POS_MSEC) 
-        current_time = timedelta(milliseconds=current_time)
-        current_time_format = f"{current_time.seconds//3600:02}:{(current_time.seconds%3600)//60:02}:{current_time.seconds%60:02}"
-
-        cv2.putText(
-                    frame,
-                    str(current_time_format),
-                    (10, 32),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    (255, 255, 255),
-                    1,
-                    cv2.LINE_AA,
-                )
-
-        if not ret:
-            log.error(" Unable to capture frame.")
-            break
-    
-        cv2.imshow("EUS Inference", frame)
-        if cv2.waitKey(FPS) & 0xFF == ord("q"):
-            log.info("Exiting.")
-            break 
 
 cap.release()
 cv2.destroyAllWindows()
+
+balanced_accuracy = balanced_accuracy_score(actual, predicted)    
+log.info(f"Balanced accuracy: {balanced_accuracy*100:.2f}%")
